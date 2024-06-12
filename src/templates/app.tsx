b3e575d4b1b42b4883cb4025777f6fd4c54c9bd0
Viki <hi@viki.moe>
2024-06-12 16:32:17 +0800

    chore: modify example code

--- src/templates/app.tsx
+++ src/templates/app.tsx
@@ -1,18 +1,27 @@
 import dayjs from 'dayjs'
 import { create } from '@shined/reactive'
+import { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const store = create({ count: 1 })
+const store = create({ count: 1, time: Date.now() })
 const addOne = () => store.mutate.count++
+const updateTime = () => (store.mutate.time = Date.now())
 
 function App() {
-  const count = store.useSnapshot(s => s.count)
-  const date = dayjs().format('YYYY/MM/DD HH:mm:ss:SSS')
+  const [count, time] = store.useSnapshot(s => [s.count, s.time])
+
+  useEffect(() => {
+    const timer = setInterval(updateTime, 1000)
+    return () => clearInterval(timer)
+  }, [])
+
+  const formatted = dayjs(time).format('YYYY/MM/DD HH:mm:ss')
 
   return (
-    <button onClick={addOne}>
-      {date} --- {count}
-    </button>
+    <div>
+      <div>Time: {formatted}</div>
+      <button onClick={addOne}>Count: {count}</button>
+    </div>
   )
 }
 
