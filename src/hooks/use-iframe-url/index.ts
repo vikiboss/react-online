c5b42242839afa1d50773d028d03f3980913c8a8
Viki <hi@viki.moe>
2024-06-19 19:32:42 +0800

    chore: modify error css

--- src/hooks/use-iframe-url/index.ts
+++ src/hooks/use-iframe-url/index.ts
@@ -1,29 +1,47 @@
-import iframe from "@/templates/iframe.html?raw";
-import { useMemo } from "react";
-import { transform } from "sucrase";
+import iframe from '@/templates/iframe.html?raw'
+import { useMemo } from 'react'
+import { transform } from 'sucrase'
 
-export function useIframeUrl(code = "", importMap = "") {
-	return useMemo(() => {
-		let html = "";
+export function useIframeUrl(code = '', importMap = '') {
+  return useMemo(() => {
+    let html = ''
 
-		try {
-			const output = transform(code, {
-				transforms: ["typescript", "jsx"],
-				jsxRuntime: "automatic",
-				production: true,
-			});
+    try {
+      const output = transform(code, {
+        transforms: ['typescript', 'jsx'],
+        jsxRuntime: 'automatic',
+        production: true,
+        filePath: 'index.js',
+        sourceMapOptions: {
+          compiledFilename: 'index.min.js',
+        },
+      })
 
-			html = getIframeContent(output.code, importMap);
-		} catch (e) {
-			html = `<h3>Error ocurred! Check your code</h3><pre>${e}</pre>`;
-		}
+      html = getIframeContent(output.code, importMap)
+    } catch (e) {
+      html = `
+    <style>
+      :root {
+        color-scheme: light dark;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+          Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      }
 
-		return URL.createObjectURL(new Blob([html], { type: "text/html" }));
-	}, [code, importMap]);
+      body {
+        margin: 0;
+        background-color: Canvas;
+        color: CanvasText;
+      }
+    </style>
+    <h3>Error ocurred! Check your code</h3>
+    <pre>${e}</pre>
+`
+    }
+
+    return URL.createObjectURL(new Blob([html], { type: 'text/html' }))
+  }, [code, importMap])
 }
 
 export function getIframeContent(script: string, importMap: string) {
-	return iframe
-		.replace("<!-- IMPORT_MAP -->", importMap)
-		.replace("/** SCRIPT */", script);
+  return iframe.replace('/** IMPORT_MAP */', importMap).replace('/** SCRIPT */', script)
 }
