1f57d65af8b4303686cf0439c68961887a249da9
Viki <hi@viki.moe>
2024-06-24 09:03:08 +0800

    chore: modify example codes

--- src/templates/app.tsx
+++ src/templates/app.tsx
@@ -1,27 +1,22 @@
 import { create } from '@shined/reactive'
-import { useMouse } from '@shined/react-use'
-import dayjs from 'dayjs'
-import { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useMouse, useDateFormat } from '@shined/react-use'
 
 const store = create({ count: 1, time: Date.now() })
 const addOne = () => store.mutate.count++
 const updateTime = () => (store.mutate.time = Date.now())
 
+setInterval(updateTime, 1000)
+
 function App() {
   const { x, y } = useMouse()
   const [count, time] = store.useSnapshot(s => [s.count, s.time])
-
-  useEffect(() => {
-    const timer = setInterval(updateTime, 1000)
-    return () => clearInterval(timer)
-  }, [])
-
-  const formatted = dayjs(time).format('YYYY/MM/DD HH:mm:ss')
+  const formatted = useDateFormat(time, 'YYYY/MM/DD HH:mm:ss')
 
   return (
     <div>
-      <div>x: {x}, y: {y}, Time: {formatted}</div>
+      <div>(x, y): ({x}, {y})</div>
+      <div>Time: {formatted}</div>
       <button onClick={addOne}>Count: {count}</button>
     </div>
   )
